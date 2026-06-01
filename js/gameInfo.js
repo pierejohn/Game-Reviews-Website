af56deb4fdf290cef2b6734f82e3de5a9a25b3ce
pierejohn <pierejohn299@gmail.com>
2026-06-01 20:04:38 +0300

    v3.6 commit

--- js/gameInfo.js
+++ js/gameInfo.js
@@ -1,54 +1,62 @@
+$(window).on("load", function () {
+    $(".loading").fadeOut(300);
+});
+
 let desiredElement = JSON.parse(localStorage.getItem("desiredElement"));
+
 if (!desiredElement) {
-    window.location.href = "./index.html";
+    $('#gameInfo').html("<h2 class='text-white'>No game selected</h2>");
+    throw new Error("No game in localStorage");
 }
 
-getDetalis(desiredElement.id);
+let itemId = desiredElement.id;
 
-let detalis = "";
+getDetalis(itemId);
+let detalis=''
+
 
 async function getDetalis(itemId) {
-    const options = {
-        method: 'GET',
-        headers: {
-            'X-RapidAPI-Key': 'YOUR_KEY',
-            'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
-        }
-    };
 
-    try {
-        let api = await fetch(
-            `https://free-to-play-games-database.p.rapidapi.com/api/game?id=${itemId}`,
-            options
-        );
 
-        detalis = await api.json();
-
-        fillItem();
-
-    } catch (err) {
-        console.log(err);
+  const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': '64b5e48e58msh1be071d2c667576p10393bjsnb84c06ed7cb4',
+      'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
     }
+  };
+
+
+    api = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/game?id=${itemId}`, options)
+    let response = await api.json()
+    
+    detalis= response
+    console.log(detalis.description);
+    fillItem()
+
 }
-function fillItem() {
 
-    $('#gameInfo').html(`
-        <div class="col-lg-4">
-            <img class="w-100" src="${desiredElement.thumbnail}">
-        </div>
+   
+    
 
-        <div class="col-lg-8 text-white">
-            <h1>Title: ${desiredElement.title}</h1>
-
-            <h6>Category: ${desiredElement.genre}</h6>
-            <h6>Platform: ${desiredElement.platform}</h6>
-            <h6>Status: Live</h6>
-
-            <p>${detalis.description}</p>
-
-            <a class="btn btn-info" target="_blank" href="${desiredElement.game_url}">
-                Play Game
-            </a>
-        </div>
-    `);
+function fillItem()
+{
+    let item=`
+<div class="col-lg-4">
+                <img class="w-100" src="${desiredElement.thumbnail}" alt="">
+            </div>
+            <div class="col-lg-8 text-white">
+                <h1 id="titel ">
+                    Title: ${desiredElement.title}
+                </h1>
+                <div class="d-flex flex-column mt-3 gap-2">
+                <h6>Category:<span class="px-2 rounded-2 ItemCaPlSta">${desiredElement.genre}</span> </h6>
+                <h6>Platform:<span class="px-2 rounded-2 ItemCaPlSta">${desiredElement.platform}</span> </h6>
+                <h6>Status:<span class="px-2 rounded-2 ItemCaPlSta">Live</span> </h6>
+                <p class="fs-6">${detalis.description}</p>
+               <a class="btn btn-info" target="_blank" href="${desiredElement.game_url}">show Game</a>
+            </div>
+            </div>
+`
+$('#gameInfo').html(item)
 }
--- js/js.js
+++ js/js.js
@@ -1,40 +1,39 @@
+
+window.onload = function () {
+    localStorage.removeItem("desiredElement");
+    window.scrollTo(0, 0);
+};
+
 let offsetbegin;
 
 $(document).ready(function () {
-
-    // safe reset (ONLY index page)
-    localStorage.removeItem("desiredElement");
-
     offsetbegin = Math.floor($('.navbar').offset().top);
-
-    getAllGames(); 
 });
 
-
-
 function removLoading() {
-    $('body').css('overflow', 'auto');
-
-    $(".loading").fadeOut(800, function () {
-        $('.loading').removeClass('d-flex');
-    });
+     $('body').css('overflow','auto');
+    $(".loading").fadeOut(1000,
+        function()
+        {
+            $('.loading').removeClass('d-flex')
+        }
+    );
 }
 
 $(window).on("load", function () {
-    removLoading();
+    removLoading()
 });
 
 function DisplayLoading() {
-    $(window).scrollTop(0);
-    $('.loading').addClass('d-flex');
+    $(window).scrollTop(0)
+    //  $('body').css('overflow','hidden');
+     $('.loading').addClass('d-flex')
     $(".loading").fadeIn(1);
 }
 
 
-
-$(window).on("scroll", function () {
+$(window).scroll(function () {
     let offsetWindow = $(window).scrollTop();
-
     if (offsetWindow >= offsetbegin) {
         $('.navbar').addClass('fixed-nav');
         $('.navbar').removeClass('translate-middle-y');
@@ -44,135 +43,137 @@ $(window).on("scroll", function () {
     }
 });
 
-
-$('.nav-link').on('click', function () {
-    DisplayLoading();
-
+$('.nav-link').click(function () {
+    // $(window).scrollTop(0)
+    DisplayLoading()
     $('.nav-link').removeClass('myActive');
     $(this).addClass('myActive');
+   
+    
+    getCategory($(this).attr('id'))
+    
+})
 
-    getCategory($(this).attr('id'));
-});
 
-$('.navbar-brand').on('click', function () {
+$('.navbar-brand').click(function () {
     $('.nav-link').removeClass('myActive');
-});
+})
 
 
-let games = [];
-let addAllItems = [];
-
+let games;
 
 async function getAllGames() {
 
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': 'YOUR_KEY',
+            'X-RapidAPI-Key': '64b5e48e58msh1be071d2c667576p10393bjsnb84c06ed7cb4',
             'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
         }
     };
+    api = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games`, options)
+    let response = await api.json();
+    games = response;
+    displayItems()
+    
+   
 
-    try {
-        let api = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games`, options);
-        let response = await api.json();
-
-        games = response;
-        displayItems();
-
-    } catch (err) {
-        console.log(err);
-    }
 }
 
 async function getCategory(Category) {
+console.log(Category);
 
     const options = {
         method: 'GET',
         headers: {
-            'X-RapidAPI-Key': 'YOUR_KEY',
+            'X-RapidAPI-Key': '64b5e48e58msh1be071d2c667576p10393bjsnb84c06ed7cb4',
             'X-RapidAPI-Host': 'free-to-play-games-database.p.rapidapi.com'
         }
     };
+    api = await fetch(`https://free-to-play-games-database.p.rapidapi.com/api/games?category=${Category}`, options)
+    let response = await api.json();
 
-    try {
-        let api = await fetch(
-            `https://free-to-play-games-database.p.rapidapi.com/api/games?category=${Category}`,
-            options
-        );
 
-        let response = await api.json();
+    games = response;
+    
+    displayItems()
+    removLoading()
 
-        games = response;
-        displayItems();
-        removLoading();
-
-    } catch (err) {
-        console.log(err);
-    }
 }
+
+getAllGames();
+let addAllItems = []
 
 
 function displayItems() {
-
-    addAllItems = "";
+    addAllItems =[]
+console.log(games);
 
     games.forEach(element => {
 
-        addAllItems += `
-        <div id="${element.id}" class="p-2 col-lg-3 col-md-4 col-sm-6 AllCards">
+
+        addAllItems += `<div id="${element.id}" class="p-2 col-lg-3 col-md-4 col-sm-6 AllCards">
 
             <div class="p-3 card-item text-white d-flex flex-column h-100 w-100 bg-dark-min">
 
+                <!-- IMAGE -->
                 <img class="w-100 rounded-2" src="${element.thumbnail}" alt="">
 
+                <!-- TITLE + PRICE -->
                 <div class="d-flex justify-content-between align-items-center py-3">
                     <h5 class="m-0">${element.title}</h5>
                     <div class="px-2 rounded-2 ItemPrice">Free</div>
                 </div>
 
+                <!-- DESCRIPTION -->
                 <p class="text-white-50">
                     ${element.short_description}
                 </p>
 
+                <!-- PUSH TO BOTTOM -->
                 <div class="mt-auto">
+                    <div class="width-90"></div>
                     <div class="d-flex justify-content-between align-items-center">
 
-                        <div class="bg-gray px-2 py-1 rounded-5">
+                        <div class="bg-gray px-2 py-1 rounded-5 d-flex justify-content-center align-items-center">
                             ${element.genre}
                         </div>
 
-                        <div class="bg-gray px-2 py-1 rounded-5">
-                            ${element.platform.length > 14
-                                ? element.platform.slice(0, 14) + "..."
-                                : element.platform}
+                        <div class="bg-gray px-2 py-1 rounded-5 d-flex justify-content-center align-items-center">
+                            ${element.platform.length > 14 
+                            ? element.platform.slice(0, 14) + "..." 
+                            : element.platform}
                         </div>
-
                     </div>
                 </div>
 
             </div>
-        </div>`;
+        </div>`
+
+
     });
 
-    $('#RowForItems').html(addAllItems);
+    $('#RowForItems').html(addAllItems)
+    addClick()
 
-    addClick();
+
+
 }
 
 function addClick() {
-
-    $('.AllCards').on('click', function () {
-
-        DisplayLoading();
-
-        const id = $(this).attr("id");
-
-        const selected = games.find(g => g.id == id);
-
-        if (selected) {
-            localStorage.setItem("desiredElement", JSON.stringify(selected));
-            window.location.href = "./pageInfo.html";
-        }
+  $('.AllCards').click(function(e){
+    DisplayLoading()
+    games.forEach(element => {
+      if(element.id==this.id)  {
+        
+        desiredElement=element
+        localStorage.setItem("desiredElement", JSON.stringify(desiredElement));
+        console.log(desiredElement);
+        window.location.href = "./pageInfo.html";
+      }
     });
+    
+}) 
+
+  
 }
